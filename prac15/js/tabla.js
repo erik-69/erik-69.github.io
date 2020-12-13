ffe5e140c13d527a09de09d1b1cae4dd51940057
Erik Maya Mondragón <71298062+erik-69@users.noreply.github.com>
2020-12-13 15:59:12 -0600

    Add files via upload

---
+++ prac15/js/tabla.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    
+    for (i=1; i<5;i++){
+        var agregarcontenido = document.createElement("tr");
+    var cadena="";
+        cadena +="<td>nombren </td><td>correon </td>";
+        cadena +="<td> <button class='btnaccion1' id='a"+ i +"'>accion1 </button>";
+        cadena +="<td> <button class='btnaccion2' id='b"+ i +"'>accion2 </button>";
+        agregarcontenido.innerHTML = cadena;
+        document.getElementById("secioncontenido").appendChild(agregarcontenido);
+
+        $("a1").on('click')
+
+
+    }
+});
